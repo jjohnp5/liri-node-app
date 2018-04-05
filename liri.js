37ee2464ea0c8f96edd9af4d7fe35fb5b9fd65b0
Lagmay <quickfire@uspicorp.com>
2018-04-05 08:53:09 -0500

    pulled code

--- liri.js
+++ liri.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 var request = require('request');
+var fs = require('fs');
 
-var spotify = new spotify(keys.spotify);
-var client = new twitter(keys.twitter);
+// var spotify = new spotify(keys.spotify);
+// var client = new twitter(keys.twitter);
+var defaultRandomSong = ""
+
+fs.readFile('random.txt',(err, data)=>{
+    if(err) throw err;
+    console.log(data.toString('utf8').replace(/"/g, '').split(','));
+})
+
 
 
